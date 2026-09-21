283d4f68637430564e849a4f0dc3c32cb257a2d5
danchu1411 <dangctde180178@fpt.edu.vn>
2026-09-21 20:38:22 +0700

    Add CategoryRepository

---
+++ Ch03_DependencyInjectionDemo/Repositories/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Ch03_DependencyInjectionDemo.Data;
+using Ch03_DependencyInjectionDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ch03_DependencyInjectionDemo.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _context;
+
+    public CategoryRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public Task<List<Category>> GetAllAsync()
+    {
+        return _context.Categories
+            .AsNoTracking()
+            .ToListAsync();
+    }
+
+    public Task<Category?> GetByIdAsync(int id)
+    {
+        return _context.Categories
+            .FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async Task AddAsync(Category category)
+    {
+        _context.Categories.Add(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Category category)
+    {
+        _context.Categories.Update(category);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+        if (category == null)
+            return;
+
+        _context.Categories.Remove(category);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ Ch03_DependencyInjectionDemo/Repositories/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Ch03_DependencyInjectionDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch03_DependencyInjectionDemo.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+    }
+}
